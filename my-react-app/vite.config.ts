c46da95b660890d62fda14c41eb397f40bc2862b
smolder1ngboy <corsarbmx2011@mail.ru>
2025-09-07 03:19:29 +0700

    Добавление Vitest в проект

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   css: {
     modules: {
-      localsConvention: "camelCase",
+      localsConvention: 'camelCase',
     },
     preprocessorOptions: {
       scss: {
